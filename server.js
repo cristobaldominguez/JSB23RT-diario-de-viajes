42d584a28dcf047201b0b08f16562dd6184c984d
Cristobal Dominguez <cristobald@gmail.com>
2023-07-18 16:03:04 +0200

    Agrega ruta 404

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const { port } = require('./config')
 const app = express()
 
 
+app.use((req, res, next) => {
+  res.status(404)
+  res.json({ error: true, msg: '404 Not Found' })
+})
 
 app.listen(port, _ => `Server running at: http://localhost:${port}`)
